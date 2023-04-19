62c06b6cc689a5e86b6cd7af57b3dad83a758bbb
Woolton <donovan.conward@gmail.com>
2023-04-19 16:46:27 -0400

    Updated menu script
    -updated menu script
    -added test graphics
    -added test audio

---
+++ Assets/CUSTOMSETUP/Donovan/scripts/MathEquation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathEquation : MathRelation
+{
+    private int term1, term2;
+
+    public MathEquation(int term1, int term2) : base(term1, term2)
+    {
+        
+    }
+
+    public override void proofRelation()
+    {
+        throw new System.NotImplementedException();
+    }
+}
